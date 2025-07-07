a63fda83e1503fae7fb6818ffc9e4718cf3c6789
Zephryr69 <110227008+Zephryr69@users.noreply.github.com>
2025-07-07 23:39:36 +0100

    🚀 Mise à jour du site

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/les-jumelles/', // ✅ Très important pour GitHub Pages
   plugins: [react()],
-});
+})
